ea6839242ca71d2b803f1bfcb7ce120b479bda1c
Tanaka1003 <tatsuya1003@outlook.jp>
2021-12-15 15:53:52 +0000

    [make]controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'relationships/create'
+  get 'relationships/destroy'
+  get 'tags/create'
+  get 'tags/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'homes#top'
+  resources :users, only: [:show, :edit, :update] do
+  end
+  resources :posts, only: [:index, :show, :new, :create, :destroy] do
+    resources :comments, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
+    resources :posts, only: [:create, :destroy]
+  end
 end
